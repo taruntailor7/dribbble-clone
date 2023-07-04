7cd63c8cc3a006a1f8f6680c1fb9832faa6e7226
Tarun Tailor <tarun@crimecheck.in>
2023-07-05 00:06:21 +0530

    working on authprovider and next auth

--- app/api/auth/[...nextauth]/routes.js
+++ app/api/auth/[...nextauth]/routes.js
@@ -1,1 +1,5 @@
 import NextAuth from "next-auth";
+
+import { authOptions } from '@/lib/session';
+
+export { handler as GET, handler as POST };
--- lib/session.ts
+++ lib/session.ts
@@ -1,6 +1,6 @@
 import { getServerSession } from "next-auth/next";
 import { NextAuthOptions, User } from "next-auth";
-import { Adapter } from "next-auth/adapters";
+import { Adapter, AdapterUser } from "next-auth/adapters";
 import GoogleProvider from 'next-auth/providers/google';
 import jsonwebtoken from 'jsonwebtoken';
 import { JWT } from 'next-auth/jwt';
@@ -8,8 +8,34 @@ import { JWT } from 'next-auth/jwt';
 export const authOptions: NextAuthOptions = {
     providers: [
         GoogleProvider({
-            clientId: '',
-            clientSecret: ''
+            clientId: process.env.GOOGLE_CLIENT_ID!,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
         })
-    ]
+    ],
+    // jwt: {
+    //     encode : ({ secret, token}) => {
+    //         return { secret, token };
+    //     },
+    //     decodea : async ({ secret, token}) => {
+    //         return { secret, token };
+    //     },
+    // },
+    theme: {
+        colorScheme: 'light',
+        logo: '/logo.png',
+    },
+    callbacks: {
+        async session({ session }) {
+            return session;
+        },
+        async signIn({ user }: { user: AdapterUser | User }) {
+            try {
+                
+                return true;
+            } catch (error:any) {
+                console.log(error, 'Error'); 
+                return false;
+            }
+        }
+    }
 }
